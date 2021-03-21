0e6b7aa2c1dedeb53e324b2393f17c30f018930c
heyuan <heyuan@yidian-inc.com>
2021-03-21 21:05:04 +0800

    hy alias ca

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -1,11 +1,14 @@
 package setting
 
 import (
-	"flag"
+	"fmt"
+	"gobee/pkg/common"
+	"io/ioutil"
 	"log"
 
 	"github.com/beego/beego/v2/core/config"
 	_ "github.com/beego/beego/v2/core/config/toml"
+	beego "github.com/beego/beego/v2/server/web"
 )
 
 var (
@@ -15,11 +18,46 @@ var (
 
 // Setup initialize the configuration instance
 func Setup() {
-	flag.StringVar(&ConfigPath, "c", "config/", "默认配置目录")
-	defaultConfigPath := ConfigPath
+	ConfigPath := beego.AppConfig.DefaultString("ConfigPath2", "conf")
 
-	err := config.InitGlobalInstance("toml", defaultConfigPath+"/mysql.toml")
-	log.Println(err)
-	val, err := config.GetSection("database")
+	if isExist := common.IsDirExist(ConfigPath); isExist == false {
+		fmt.Println("配置目录路径:", ConfigPath, "不存在,以加载默认配置启用:", ConfigPath, "请检查配置")
+	}
+
+	files, _ := ioutil.ReadDir(ConfigPath)
+	// var b bytes.Buffer
+	for _, f := range files {
+		fmt.Println(f.Name())
+		// if ok := beego.LoadAppConfig("ini", ConfigPath+"/"+f.Name()); ok != nil {
+		// 	log.Println("配置文件:", f.Name(), "不存在,请检查配置文件")
+		// }
+		// if ok := config.InitGlobalInstance("toml", ConfigPath+"/"+f.Name()); ok != nil {
+		// 	log.Println("配置文件:", f.Name(), "不存在,请检查配置文件")
+		// }
+		// ctx2, _ := ioutil.ReadFile(ConfigPath + "/" + f.Name())
+
+		// fmt.Println(ctx2)
+		// ww, ok := b.Write(ctx2)
+	}
+
+	ctx2, _ := ioutil.ReadFile(ConfigPath + "/mysql.toml")
+	config.NewConfigData("toml", ctx2)
+
+
+	val, err := config.GetSection("Mysql")
 	log.Println(val, err)
+	val2, err := config.GetSection("Mongo.ShenbianUgc")
+	log.Println(val2, err)
+
+	// val3, err := beego.AppConfig.GetSection("mongo.shenbian_ugc")
+	// log.Println(val3, err)
+	// val4, err := beego.AppConfig.GetSection("mysql.shenbian_ugc")
+	// log.Println(val4, err)
+
+	// val5, err := beego.AppConfig.GetSection("shenbianugc")
+	// log.Println(val5, err)
+
+	// val6, err := beego.AppConfig.String("ConfigPath")
+	// log.Println(val6, err)
+
 }
